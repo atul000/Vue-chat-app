19f701359adb640107f4d46d164060a4212d844f
atul beniwal <33995896+atul000@users.noreply.github.com>
2020-01-26 18:43:20 +0530

    version 2

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,32 +1,51 @@
 <template>
-  <div class="hello">
-    <h2>welcome</h2>
+  <div class="welcome container">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="teal-text">Welcome</h2>
+        <form @submit.prevent="enterChat">
+          <label for="name">Enter your name:</label>
+          <input type="text" name="name" v-model="name" />
+          <p v-if="feedback" class="red-text">
+            {{ feedback }}
+          </p>
+          <button class="btn teal">Enter Chat</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
+  name: "Welcome",
+  data() {
+    return {
+      name: null,
+      feedback: null
+    };
+  },
+  methods: {
+    enterChat() {
+      if (this.name) {
+        this.$router.push({ name: "Chat", params: { name: this.name } });
+      } else {
+        this.feedback = "You must enter a name to join";
+      }
+    }
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.welcome {
+  max-width: 400px;
+  margin-top: 100px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.welcome h2 {
+  font-size: 3em;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.welcome button {
+  margin: 30px auto;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Welcome from "@/components/Welcome";
+import Chat from "@/components/Chat";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,12 @@ const routes = [
     path: "/",
     name: "Welcome",
     component: Welcome
+  },
+  {
+    path: "/chat",
+    name: "Chat",
+    component: Chat,
+    props: true
   }
 ];
 
